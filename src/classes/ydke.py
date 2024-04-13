656fbffc87bbb1c2050b0836b0334bd03dae39d7
Matheus <matheus.limacunha@hotmail.com>
2024-04-13 11:06:05 -0300

    Add YDKE decoder

---
+++ src/classes/ydke.py
@@ -0,0 +1,27 @@
+from array import array
+from base64 import b64decode
+
+class YDKE:
+    URL_START = "ydke://"
+    SEPARATOR = "!"
+
+    @staticmethod
+    def DecodeYDKE(url : str) -> list:
+        if not url.startswith(YDKE.URL_START):
+            return None
+
+        url = url.removeprefix(YDKE.URL_START)
+        decks = url.split(YDKE.SEPARATOR)
+
+        if len(decks) < 3:
+            return None
+
+        passcodes = []
+        # 0 for main, 1 for extra, and 2 for side
+        for i in range(0, 3):
+            # ydke files are stored in base64 unsigned ints
+            decklist = array("I")
+            decklist.frombytes(b64decode(decks[i]))
+            passcodes.append(decklist)
+        
+        return passcodes
